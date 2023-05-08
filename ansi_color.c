120a924b7f9b19f8f9c88135dad966c3d3d2e67f
ketch <ketchu15@stud.thga.de>
2023-05-08 15:17:35 +0200

    added ansi_color.c

--- ansi_color.c
+++ ansi_color.c
@@ -12,19 +12,6 @@ int main() {
                      "    > ^ < ";
     printColoredAsciiArt(asciiArt);
     return 0;
-}#include <stdio.h>
+}
 
-void printColoredAsciiArt(char* asciiArt) {
-	    // Text in rot formatieren
-	    //     printf("\033[31m%s\033[0m", asciiArt);
-	    //     }
-	    //
-	    //     int main() {
-	    //         char* asciiArt = "    /\\_/\\ \n"
-	    //                              "   ( o.o )\n"
-	    //                                                   "    > ^ < ";
-	    //                                                       printColoredAsciiArt(asciiArt);
-	    //                                                           return 0;
-	    //                                                           }
-	    //
 
